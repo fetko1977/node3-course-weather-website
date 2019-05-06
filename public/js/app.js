571ae26411b7fc55382fe18882e8ef460c47459f
Svetoslav Savov <svetoslav.savov@stanga.net>
2019-05-06 13:01:55 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,27 @@
+console.log('Client js file is loaded')
+
+const weatherForm = document.querySelector('form')
+const searchInput = document.querySelector('form input')
+const messageOne = document.getElementById('message-one')
+const messageTwo = document.getElementById('message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchInput.value
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forcast = (latitude, longtitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/4a25d3a630feb718bbe53da8173ed962/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longtitude) + '?units=si'
+    
+    request({
+        url,
+        json: true
+    }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to forecast service', undefined)
+        } else if (body.error) {
+            callback(body.error, undefined)
+        } else {
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.`)
+        }
+    })
+}
+
+module.exports = forcast
